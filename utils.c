b4c0210cbfde62b6803cc852130eac01d64ff0c2
Lyliafrgn <151639945+Lyliafrgn@users.noreply.github.com>
2025-04-26 00:54:22 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -106,3 +106,23 @@ static int	ft_get_token_size(char *line)
 	}
 	return (len);
 }
+
+
+t_token	*ft_findlast_token(t_token *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
+
+t_token	*ft_token_before_last(t_token *list)
+{
+	t_token	*cur_token;
+
+	cur_token = list;
+	while (cur_token->next->next != NULL)
+		cur_token = cur_token->next;
+	return (cur_token);
+}

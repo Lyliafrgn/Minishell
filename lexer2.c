530a8f34d8246b4993c5c314672c3502f180c4c8
Lyliafrgn <151639945+Lyliafrgn@users.noreply.github.com>
2025-04-26 00:52:58 +0200

    Update lexer2.c

--- lexer2.c
+++ lexer2.c
@@ -57,3 +57,71 @@ int	ft_lexer(t_data *data)
 	ft_expand(data, &data->token_list);
 	return (SUCCESS);
 }
+
+
+static int	ft_is_error(t_data *data, t_token *list, t_token *tok)
+{
+	int	return_value;
+
+	if (!list || !tok)
+		return (NO);
+	return_value = ft_first_checks(data, tok);
+	if (return_value != SKIP)
+		return (return_value);
+	if (tok->type == PIPE)
+	{
+		if (tok->next && ft_isop(tok->next->value) == YES)
+		{
+			if (tok->next->next && tok->next->next->type == PIPE)
+				return (ft_err(tok->next, tok->next->next->type),
+					ft_status(data), YES);
+			else if (ft_isop(tok->next->value) == YES
+				&& tok->next->next == NULL)
+				return (ft_err(tok->next, NEWLINE_ERROR), ft_status(data), YES);
+		}
+	}
+	if (ft_isop(tok->value) == YES && tok->next != NULL
+		&& tok->next->type == PIPE)
+		return (ft_err(tok, tok->next->type), ft_status(data), YES);
+	if (ft_isoperator(tok->value) >= 1 && tok->next == NULL)
+		return (ft_err(tok, NEWLINE_ERROR), ft_status(data), YES);
+	return (NO);
+}
+
+static int	ft_double_pipe_detected(t_data *data, t_token *token)
+{
+	while (token)
+	{
+		if (token->type == PIPE && token->next && token->next->type == PIPE)
+			return (ft_status(data), YES);
+		token = token->next;
+	}
+	return (NO);
+}
+
+int	ft_check_token_list(t_data *data, t_token *list)
+{
+	t_token	*cur_token;
+	t_token	*last_token;
+
+	cur_token = list;
+	if (!cur_token)
+		return (SUCCESS);
+	last_token = ft_findlast_token(list);
+	if (ft_double_pipe_detected(data, cur_token) == YES)
+		return (ft_print_err(DOUBLE_PIPE_ERROR), FAIL);
+	if (cur_token->value && cur_token->value[0] == '|')
+		return (ft_print_err(PIPE), ft_status(data), FAIL);
+	while (cur_token != NULL)
+	{
+		if (ft_is_error(data, list, cur_token) == YES)
+			break ;
+		cur_token = cur_token->next;
+	}
+	if (cur_token == NULL)
+	{
+		if (last_token->type == PIPE)
+			return (ft_err(ft_token_before_last(list), PIPE_AT_END), SUCCESS);
+	}
+	return (SUCCESS);
+}

7cbbe86abd24adebfda4a936379f5a9c3d8f767d
Lyliafrgn <151639945+Lyliafrgn@users.noreply.github.com>
2025-04-26 00:40:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -18,24 +18,15 @@ int	main(int ac, char **av, char **env)
 	t_data *const	data = ft_create_data(ac, av, env);
 	int				s;
 
-	if (!data || ac != 1)
-		return (-1);
 	while (1)
 	{
-		signal(SIGINT, &ft_standard_handler);
-		signal(SIGQUIT, SIG_IGN);
-		data->line = readline(MINISPELL);
+		data->line = readline("prompt >");
 		if (!data->line)
 			break ;
-		ft_exitstatus_signal(data);
 		if (data->line[0] != '\0')
 			add_history(data->line);
 		if (ft_lexer(data) == SUCCESS && ft_parser(data) == SUCCESS)
-			ft_launch_exec(data);
-		ft_wait_for_children(data);
-		ft_reset_data(data);
+			launch_exec(data);
 	}
-	if (isatty(STDIN_FILENO))
-		write(2, "exit\n", 6);
 	return (s = data->exit_status, rl_clear_history(), ft_free_all(data), s);
 }
